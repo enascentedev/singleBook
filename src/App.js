fcb4c5b23f73ad04af98c1860202c60c9a098737
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2023-08-21 00:37:28 -0300

    Uso de propspara criar componentes dinâmicos

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import Header from "./componentes/Header";
+import Pesquisa from "./componentes/Pesquisa";
+import UltimosLancamentos from "./componentes/UltimosLancamento";
 import styled from "styled-components";
 
 const AppContainer = styled.div`
 	width: 100vw;
 	height: 100vh;
-	background-image: linear-gradient(90deg, #002f52 35%, #326589);
-	list-style: none;
+	background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
 `;
 
 function App() {
 	return (
 		<AppContainer>
 			<Header />
+			<Pesquisa />
+			<UltimosLancamentos />
 		</AppContainer>
 	);
 }
